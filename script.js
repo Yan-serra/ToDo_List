f67b0486579bf304dd21996cecc5ba4f332c72ed
Yan-serra <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>
2026-03-20 08:54:34 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 let tarefas= JSON.parse(localStorage.getItem("tarefa")) ||[];
 
+function salvaTarefa(){
+    localStorage.setItem("tarefa", JSON.stringify(tarefa));
+}
+
 function  addTarefa(){
     let titulo = document.getElementById("titulo").value;
     let descricaotarefa = document.getElementById("descricaotarefa").value;
     let prioridades = document.getElementById("prioridades").value;
 
-    if(titulo && descricaotarefa && prioridades){
+    if(!titulo || !descricaotarefa || !prioridades){
         alert("Preencha todos os campos por gentileza!");
         return;
     }
 }
 
-let data = new Date().toDateString();
+let data = new Date().toDateString('pt-BR');
 
 let tarefa={
     titulo, descricaotarefa,prioridades, data, status:"Pendente | Em andamento| Finalizada"
 }
 
-tarefa.push(tarefa);
-addTarefa();
+tarefas.push(tarefa);
+
+salvaTarefa();
 lista();
 
-document.getElementById("titulo").value = " ";
-document.getElementById("descricaotarefa").value = " ";
-document.getElementById("prioridades").value = " ";
-
-function salvaTarefa(){
-    localStorage.setItem("tarefa", JSON.stringify(tarefa));
-}
+document.getElementById("titulo").value = "";
+document.getElementById("descricaotarefa").value = "";
 
 
 function lista(){
     let lista = document.getElementById("lista");
     lista.innerHTML = " ";
 
-    tarefas.forEach((tarefa,Index)=> {
-        let iten = document.createElement("iten");
-        iten.innerHTML = `
+    tarefas.forEach((tarefa,index)=> {
+        let item = document.createElement("li");
+        
+        item.innerHTML = `
         <h3>${tarefa.titulo}</h3>
         <p>${tarefa.descricaotarefa}</p>
         <p>Prioridade: ${tarefa.prioridades}</p>
